2ec773124f8887b31b6a78dc2ef4fc234bc59695
BruceCC <1804284906@qq.com>
2018-02-08 14:42:24 +0800

    添加基础框架

--- src/main/java/org/leave/chapter2/service/CustomerService.java
+++ src/main/java/org/leave/chapter2/service/CustomerService.java
@@ -5,6 +5,8 @@ import org.leave.chapter2.util.PropsUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
@@ -20,9 +22,9 @@ public class CustomerService  {
     static {
         Properties conf = PropsUtil.loadProps("config.properties");
         DRIVER = conf.getProperty("jdbc.driver");
-        URL = conf.getProperty("jjdbc.url");
-        USERNAME = conf.getProperty("username");
-        PASSWORD = conf.getProperty("password");
+        URL = conf.getProperty("jdbc.url");
+        USERNAME = conf.getProperty("jdbc.username");
+        PASSWORD = conf.getProperty("jdbc.password");
 
         try{
             Class.forName(DRIVER);
@@ -32,7 +34,35 @@ public class CustomerService  {
     }
 
     public List<Customer> getCustomerList(){
-        //TODO
+        Connection conn = null;
+        try{
+            List<Customer> customerList = new ArrayList<Customer>();
+            String sql = "select * from customer";
+            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+            while(rs.next()){
+                Customer customer = new Customer();
+                customer.setId(rs.getLong("id"));
+                customer.setName(rs.getString("name"));
+                customer.setContact(rs.getString("contact"));
+                customer.setTelephone(rs.getString("telephone"));
+                customer.setEmail(rs.getString("email"));
+                customer.setRemark(rs.getString("remark"));
+                customerList.add(customer);
+            }
+            return customerList;
+        } catch (SQLException e){
+            LOGGER.error("execute sql failure !", e);
+        } finally {
+            if(conn != null){
+                try{
+                    conn.close();
+                } catch (SQLException e){
+                    LOGGER.error("Close collection failure !", e);
+                }
+            }
+        }
         return null;
     }
 
