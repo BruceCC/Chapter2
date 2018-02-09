6423a9d759f5528c4d360990f8c70acba9c7ff7a
BruceCC <1804284906@qq.com>
2018-02-09 14:34:22 +0800

    添加页面框架

---
+++ src/main/java/org/leave/chapter2/controller/CustomerServlet.java
@@ -0,0 +1,29 @@
+package org.leave.chapter2.controller;
+
+import org.leave.chapter2.model.Customer;
+import org.leave.chapter2.service.CustomerService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/customer")
+public class CustomerServlet extends HttpServlet{
+    private CustomerService customerService;
+
+    @Override
+    public void init() throws ServletException {
+        customerService = new CustomerService();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        List<Customer> customerList = customerService.getCustomerList();
+        req.setAttribute("customerList", customerList);
+        req.getRequestDispatcher("WEB-INF/jsp/customer.jsp").forward(req, resp);
+    }
+}
